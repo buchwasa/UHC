105107afa89ec409fdf2dd3971866acd7d64e0f1
Drew <buchwalddrew@icloud.com>
2020-03-19 00:04:02 -0400

    Changed how scenarios are done, they no longer are done within the plugin and can be installed into the UHC configuration folder.

--- src/uhc/Loader.php
+++ src/uhc/Loader.php
@@ -8,19 +8,7 @@ use uhc\command\HealCommand;
 use uhc\command\ScenariosCommand;
 use uhc\command\SpectatorCommand;
 use uhc\command\UHCCommand;
-use uhc\scenario\AppleRates;
-use uhc\scenario\Barebones;
-use uhc\scenario\BloodDiamond;
-use uhc\scenario\CatEyes;
-use uhc\scenario\CutClean;
-use uhc\scenario\DoubleHealth;
-use uhc\scenario\DoubleOres;
-use uhc\scenario\DoubleOrNothing;
-use uhc\scenario\EnchantedDeath;
-use uhc\scenario\Fireless;
-use uhc\scenario\HeadPole;
-use uhc\scenario\NoFall;
-use uhc\scenario\Scenario;
+use uhc\Scenario;
 
 class Loader extends PluginBase{
 
@@ -34,10 +22,11 @@ class Loader extends PluginBase{
 	private $scenarios = [];
 
 	public function onEnable() : void{
+		if(!is_dir($this->getDataFolder() . "scenarios")){
+			mkdir($this->getDataFolder() . "scenarios");
+		}
 		$this->getScheduler()->scheduleRepeatingTask(new UHCTimer($this), 20);
 		new EventListener($this);
-
-		$this->registerScenarios();
 		
 		$this->getServer()->getCommandMap()->registerAll("uhc", [
 			new UHCCommand($this),
@@ -45,6 +34,15 @@ class Loader extends PluginBase{
 			new SpectatorCommand($this),
 			new HealCommand($this)
 		]);
+		
+		foreach(scandir($this->getDataFolder() . "scenarios") as $files){
+			if(substr($files, -4) === ".php"){
+				require($this->getDataFolder() . "scenarios/" . $files);
+				$scenario = str_replace(".php", "", $files);
+				$class = "\\$scenario";
+				$this->addScenario(new $class($this));
+			}
+		}
 	}
 
 	public function addElimination(Player $player) : void{
@@ -70,20 +68,5 @@ class Loader extends PluginBase{
 
 	public function addScenario(Scenario $scenario) : void{
 		$this->scenarios[$scenario->getName()] = $scenario;
-	}
-
-	private function registerScenarios() : void{
-		$this->addScenario(new AppleRates($this));
-		$this->addScenario(new Barebones($this));
-		$this->addScenario(new BloodDiamond($this));
-		$this->addScenario(new CatEyes($this));
-		$this->addScenario(new CutClean($this));
-		$this->addScenario(new DoubleHealth($this));
-		$this->addScenario(new DoubleOres($this));
-		$this->addScenario(new DoubleOrNothing($this));
-		$this->addScenario(new EnchantedDeath($this));
-		$this->addScenario(new Fireless($this));
-		$this->addScenario(new HeadPole($this));
-		$this->addScenario(new NoFall($this));
 	}
 }
--- src/uhc/Scenario.php
+++ src/uhc/Scenario.php
@@ -1,10 +1,9 @@
 <?php
 declare(strict_types=1);
 
-namespace uhc\scenario;
+namespace uhc;
 
 use pocketmine\event\Listener;
-use uhc\Loader;
 
 class Scenario implements Listener{
 	/** @var Loader */
