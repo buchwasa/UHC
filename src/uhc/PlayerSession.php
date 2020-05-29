6d0558d6b13ca997cab6d73170dbc44d2db0e34f
Wumpotamus <17461354+Wumpotamus@users.noreply.github.com>
2020-05-28 20:40:53 -0400

    Added null typehint to $teams in PlayerSession.

--- src/uhc/PlayerSession.php
+++ src/uhc/PlayerSession.php
@@ -15,7 +15,7 @@ class PlayerSession{
 	private $player;
 	/** @var int[] */
 	private $eliminations = [];
-	/** @var Team */
+	/** @var Team|null */
 	private $team = null;
 
 	public function __construct(Player $player){
