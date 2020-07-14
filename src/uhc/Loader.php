9a5a50c040a1f5ae4a33f8ae353f1583218f2440
Stephen <17461354+buchwasa@users.noreply.github.com>
2020-07-13 22:25:21 -0400

    Catch and handle throwables when loading the Scenario, rather than crashing.

--- src/uhc/Loader.php
+++ src/uhc/Loader.php
@@ -14,6 +14,7 @@ use uhc\command\TpallCommand;
 use uhc\command\UHCCommand;
 use uhc\game\GameHeartbeat;
 use uhc\game\Scenario;
+use Throwable;
 use function is_array;
 use function is_dir;
 use function mkdir;
@@ -21,145 +22,131 @@ use function scandir;
 use function str_replace;
 use function substr;
 
-class Loader extends PluginBase
-{
-    /** @var GameHeartbeat */
-    private $heartbeat;
-    /** @var Player[] */
-    private $gamePlayers = [];
-    /** @var PlayerSession[] */
-    private $sessions = [];
-    /** @var bool */
-    private $globalMuteEnabled = false;
-    /** @var Scenario[] */
-    private $scenarios = [];
+class Loader extends PluginBase{
+	/** @var GameHeartbeat */
+	private $heartbeat;
+	/** @var Player[] */
+	private $gamePlayers = [];
+	/** @var PlayerSession[] */
+	private $sessions = [];
+	/** @var bool */
+	private $globalMuteEnabled = false;
+	/** @var Scenario[] */
+	private $scenarios = [];
 
-    public function onEnable(): void
-    {
-        if (!is_dir($this->getDataFolder() . "scenarios")) {
-            mkdir($this->getDataFolder() . "scenarios");
-        }
-        $this->heartbeat = new GameHeartbeat($this);
-        $this->getScheduler()->scheduleRepeatingTask($this->heartbeat, 20);
-        new EventListener($this);
+	public function onEnable() : void{
+		if(!is_dir($this->getDataFolder() . "scenarios")){
+			mkdir($this->getDataFolder() . "scenarios");
+		}
+		$this->heartbeat = new GameHeartbeat($this);
+		$this->getScheduler()->scheduleRepeatingTask($this->heartbeat, 20);
+		new EventListener($this);
 
-        $this->getServer()->getCommandMap()->registerAll("uhc", [
-            new UHCCommand($this),
-            new ScenariosCommand($this),
-            new SpectatorCommand($this),
-            new HealCommand($this),
-            new GlobalMuteCommand($this),
-            new TpallCommand($this)
-        ]);
-        $this->loadScenarios();
-    }
+		$this->getServer()->getCommandMap()->registerAll("uhc", [
+			new UHCCommand($this),
+			new ScenariosCommand($this),
+			new SpectatorCommand($this),
+			new HealCommand($this),
+			new GlobalMuteCommand($this),
+			new TpallCommand($this)
+		]);
+		$this->loadScenarios();
+	}
 
-    public function loadScenarios(): void
-    {
-        $dir = scandir($this->getDataFolder() . "scenarios");
-        if (is_array($dir)) {
-            foreach ($dir as $file) {
-                $fileLocation = $this->getDataFolder() . "scenarios/" . $file;
-                if (substr($file, -4) === ".php") {
-                    require($fileLocation);
-                    $class = "\\" . str_replace(".php", "", $file);
-                    if (($scenario = new $class($this)) instanceof Scenario) {
-                        $this->addScenario($scenario);
-                    }
-                }
-            }
-        }
-    }
+	public function loadScenarios() : void{
+		$dir = scandir($this->getDataFolder() . "scenarios");
+		if(is_array($dir)){
+			foreach($dir as $file){
+				$fileLocation = $this->getDataFolder() . "scenarios/" . $file;
+				if(substr($file, -4) === ".php"){
+					try{
+						require($fileLocation);
+						$class = "\\" . str_replace(".php", "", $file);
+						if(($scenario = new $class($this)) instanceof Scenario){
+							$this->addScenario($scenario);
+						}
+					}catch(Throwable $error){
+						$this->getLogger()->error("Failed to load $file, reason: " . $error);
+					}
+				}
+			}
+		}
+	}
 
-    public function getHeartbeat(): GameHeartbeat
-    {
-        return $this->heartbeat;
-    }
+	public function getHeartbeat() : GameHeartbeat{
+		return $this->heartbeat;
+	}
 
-    public function setGlobalMute(bool $enabled): void
-    {
-        $this->globalMuteEnabled = $enabled;
-    }
+	public function setGlobalMute(bool $enabled) : void{
+		$this->globalMuteEnabled = $enabled;
+	}
 
-    public function isGlobalMuteEnabled(): bool
-    {
-        return $this->globalMuteEnabled;
-    }
+	public function isGlobalMuteEnabled() : bool{
+		return $this->globalMuteEnabled;
+	}
 
-    public function addToGame(Player $player): void
-    {
-        if (!isset($this->gamePlayers[$player->getUniqueId()->toString()])) {
-            $this->gamePlayers[$player->getUniqueId()->toString()] = $player;
-        }
-    }
+	public function addToGame(Player $player) : void{
+		if(!isset($this->gamePlayers[$player->getUniqueId()->toString()])){
+			$this->gamePlayers[$player->getUniqueId()->toString()] = $player;
+		}
+	}
 
-    public function removeFromGame(Player $player): void
-    {
-        if (isset($this->gamePlayers[$player->getUniqueId()->toString()])) {
-            unset($this->gamePlayers[$player->getUniqueId()->toString()]);
-        }
-    }
+	public function removeFromGame(Player $player) : void{
+		if(isset($this->gamePlayers[$player->getUniqueId()->toString()])){
+			unset($this->gamePlayers[$player->getUniqueId()->toString()]);
+		}
+	}
 
-    /**
-     * @return Player[]
-     */
-    public function getGamePlayers(): array
-    {
-        return $this->gamePlayers;
-    }
+	/**
+	 * @return Player[]
+	 */
+	public function getGamePlayers() : array{
+		return $this->gamePlayers;
+	}
 
-    public function isInGame(Player $player): bool
-    {
-        return isset($this->gamePlayers[$player->getUniqueId()->toString()]);
-    }
+	public function isInGame(Player $player) : bool{
+		return isset($this->gamePlayers[$player->getUniqueId()->toString()]);
+	}
 
-    public function addSession(PlayerSession $session): void
-    {
-        if (!isset($this->sessions[$session->getUniqueId()->toString()])) {
-            $this->sessions[$session->getUniqueId()->toString()] = $session;
-        }
-    }
+	public function addSession(PlayerSession $session) : void{
+		if(!isset($this->sessions[$session->getUniqueId()->toString()])){
+			$this->sessions[$session->getUniqueId()->toString()] = $session;
+		}
+	}
 
-    public function removeSession(PlayerSession $session): void
-    {
-        if (isset($this->sessions[$session->getUniqueId()->toString()])) {
-            unset($this->sessions[$session->getUniqueId()->toString()]);
-        }
-    }
+	public function removeSession(PlayerSession $session) : void{
+		if(isset($this->sessions[$session->getUniqueId()->toString()])){
+			unset($this->sessions[$session->getUniqueId()->toString()]);
+		}
+	}
 
-    public function hasSession(Player $player): bool
-    {
-        return isset($this->sessions[$player->getUniqueId()->toString()]);
-    }
+	public function hasSession(Player $player) : bool{
+		return isset($this->sessions[$player->getUniqueId()->toString()]);
+	}
 
-    /**
-     * @return PlayerSession[]
-     */
-    public function getSessions(): array
-    {
-        return $this->sessions;
-    }
+	/**
+	 * @return PlayerSession[]
+	 */
+	public function getSessions() : array{
+		return $this->sessions;
+	}
 
-    public function getSession(Player $player): ?PlayerSession
-    {
-        return $this->hasSession($player) ? $this->sessions[$player->getUniqueId()->toString()] : null;
-    }
+	public function getSession(Player $player) : ?PlayerSession{
+		return $this->hasSession($player) ? $this->sessions[$player->getUniqueId()->toString()] : null;
+	}
 
-    /**
-     * @return Scenario[]
-     */
-    public function getScenarios(): array
-    {
-        return $this->scenarios;
-    }
+	/**
+	 * @return Scenario[]
+	 */
+	public function getScenarios() : array{
+		return $this->scenarios;
+	}
 
-    public function getScenario(string $scenarioName) : Scenario
-    {
-        return $this->scenarios[$scenarioName];
-    }
+	public function getScenario(string $scenarioName) : Scenario{
+		return $this->scenarios[$scenarioName];
+	}
 
-    public function addScenario(Scenario $scenario): void
-    {
-        $this->scenarios[$scenario->getName()] = $scenario;
-    }
+	public function addScenario(Scenario $scenario) : void{
+		$this->scenarios[$scenario->getName()] = $scenario;
+	}
 }
