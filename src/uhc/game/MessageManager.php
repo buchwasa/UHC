92fb72adfb4bb87fc02ee3a8d8cfab18c446702b
nolimitTru <nedalibrahim982@gmail.com>
2021-03-11 00:01:29 -0500

    Message messager to handle death messages etc

--- src/uhc/game/MessageManager.php
+++ src/uhc/game/MessageManager.php
@@ -1,10 +1,32 @@
 <?php
 
+declare(strict_types=1);
 
 namespace uhc\game;
 
+use uhc\UHC;
 
 class MessageManager
 {
+	/** @var UHC */
+	private UHC $plugin;
+
+	public function __construct(UHC $plugin)
+	{
+		$this->plugin = $plugin;
+	}
+
+	/**
+	 * @return UHC
+	 */
+	public function getPlugin(): UHC
+	{
+		return $this->plugin;
+	}
+
+	public function getDeathMessages(): array
+	{
+		return $this->getPlugin()->config->get("death-messages");
+	}
 
 }
